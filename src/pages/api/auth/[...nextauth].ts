43dc248484f1e99c3ee14d0ded01cc25f69247db
sukpo61 <86722178+sukpo61@users.noreply.github.com>
2023-06-24 16:58:55 +0900

    세션 비활성화

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,12 +5,11 @@ import { useRouter } from "next/router";
 import api from "../../../axios";
 
 export const authOptions: NextAuthOptions = {
-  secret: "b7cae8dcb0fb3e997e75ac804d01c492",
+  secret: process.env.SECRET,
   providers: [
     GoogleProvider({
-      clientId:
-        "249263652098-3uqunp8nck6c7cot7me7n9t6u8048j3k.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   callbacks: {
@@ -27,22 +26,22 @@ export const authOptions: NextAuthOptions = {
         console.log(error);
       }
     },
-    session: async ({ session, token }: any) => {
-      if (session?.user) {
-        session.user.id = token.uid;
-      }
-      return session;
-    },
-    jwt: async ({ user, token }) => {
-      if (user) {
-        token.uid = user.id;
-      }
-      return token;
-    },
+    // session: async ({ session, token }: any) => {
+    //   if (session?.user) {
+    //     session.user.id = token.uid;
+    //   }
+    //   return session;
+    // },
+    // jwt: async ({ user, token }) => {
+    //   if (user) {
+    //     token.uid = user.id;
+    //   }
+    //   return token;
+    // },
   },
-  session: {
-    strategy: "jwt",
-  },
+  // session: {
+  //   strategy: "jwt",
+  // },
 };
 
 export default NextAuth(authOptions);
